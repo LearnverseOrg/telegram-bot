939fac48c960b1123dc0e6318040fabd55717f35
parthsali <parthsali04@gmail.com>
2025-12-10 20:40:54 +0530

    feat: Add and start self-ping functionality to keep the server awake.

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import app from "./src/app.js";
 import { PORT } from "./src/config/config.js";
-
+import { startSelfPing } from "./src/helpers/self-ping.js";
 import { connectDB } from "./src/config/db.js";
 
 const startServer = () => {
@@ -8,6 +8,7 @@ const startServer = () => {
     console.log(`Server is running on port ${PORT}`);
     connectDB();
 
+    startSelfPing();
     // startCronJob();
   });
 };
